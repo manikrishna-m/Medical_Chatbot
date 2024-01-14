75e771ad2c810517162f5b1e3a5562e447c7a73c
Mani Krishna Mandepudi <manikrishnamandepudi@Manis-MacBook-Pro.local>
2024-01-14 20:38:03 +0000

    Added folder structure for the project

---
+++ template.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+list_of_files = [
+    'src/__init__.py',
+    'src/helpper.py',
+    'src/prompt.py',
+    '.env',
+    'setup.py',
+    'app.py',
+    # 'research/trails.ipynb'
+    'store_index.py',
+    'static',
+    'templates/chat_model.html'
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != '':
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Creating directory : {filedir} for the file : {filename}')
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filename)):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f'Creating empty file: {filepath}')
+    else:
+        logging.info(f'{filename} is already exist')
