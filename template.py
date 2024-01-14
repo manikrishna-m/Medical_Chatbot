e1e6fb6dad46dbf51401dd80df649a7921da0129
Mani Krishna Mandepudi <manikrishnamandepudi@Manis-MacBook-Pro.local>
2024-01-14 20:48:05 +0000

    Made changes to the folder structure

--- template.py
+++ template.py
@@ -14,8 +14,8 @@ list_of_files = [
     'app.py',
     # 'research/trails.ipynb'
     'store_index.py',
-    'static',
-    'templates/chat_model.html'
+    'static/.gitkeep',
+    'templates/chat.html'
 ]
 
 for filepath in list_of_files:
@@ -26,7 +26,7 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f'Creating directory : {filedir} for the file : {filename}')
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filename)):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
             logging.info(f'Creating empty file: {filepath}')
