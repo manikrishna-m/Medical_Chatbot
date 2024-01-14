c4d8ba5f09f4797a03d687abb893a801f8142607
Mani Krishna Mandepudi <manikrishnamandepudi@Manis-MacBook-Pro.local>
2024-01-14 20:59:02 +0000

    Added code for the setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Mani Krishna Mandepudi',
+    author_email= 'madepudi.mk@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
